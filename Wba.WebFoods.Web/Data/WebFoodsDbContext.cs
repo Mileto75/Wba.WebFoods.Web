7226653232fb835073ee8900018d7b6b82fd9d12
Miledi75 <mileto.di.marco@howest.be>
2023-10-23 10:30:00 +0200

    Add project files.

---
+++ Wba.WebFoods.Web/Data/WebFoodsDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Wba.WebFoods.Core.Entities;
+
+namespace Wba.WebFoods.Web.Data
+{
+    public class WebFoodsDbContext : DbContext
+    {
+        //define DbSets that represent tables
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Property> Properties { get; set; }
+        public WebFoodsDbContext(DbContextOptions<WebFoodsDbContext> 
+            options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
